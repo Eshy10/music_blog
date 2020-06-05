73d5720bd260d6f1fcd7e83761f5f297cbfb497e
modupe <adewalebose15@gmail.com>
2020-06-05 15:14:02 +0100

    Add login and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'categories/show'
   get 'articles/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new', as: 'signup'
   get 'users/create'
   root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
